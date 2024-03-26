c99ac3c5fec693f646fbc6e949353db09e0cbae9
SHUBHAM  BHADARAGE <136561695+Shubhamb13@users.noreply.github.com>
2024-03-27 02:06:53 +0530

    Update App.jsx
    for dark mode comment out Lightcom component and Lighthome component and add Home and component.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Lightcom from './Intern/Lightcom'
-import Lighthome from './Intern/Lighthome'
+// import Lightcom from './Intern/Lightcom'
+// import Lighthome from './Intern/Lighthome'
+import Home from './Intern/Home'
+import Component from './Intern/Component'
 
 const App = () => {
   return (
     <div className='App'>
-      <Lighthome/>
-       <Lightcom/>
+{/*       <Lighthome/>
+       <Lightcom/> */}
+      <Home/>
+      <Component/>
     </div>
   )
 }
